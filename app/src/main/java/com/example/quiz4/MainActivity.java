fe1ed46f5b502ad8130b5f3b8cf48f414ce5f267
mertkanak <mertkanak77@icloud.com>
2023-11-13 10:08:20 +0300

    ui fixed

--- app/src/main/java/com/example/quiz4/MainActivity.java
+++ app/src/main/java/com/example/quiz4/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.quiz4;
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -20,10 +24,8 @@ public class MainActivity extends Activity {
 
         final EditText liraAmountEditText = findViewById(R.id.liraAmount);
         final EditText convertedAmountEditText = findViewById(R.id.convertedAmount);
-        Button convertButton = findViewById(R.id.convertButton);
-        Button resetButton = findViewById(R.id.resetButton);
+        ListView currencyListView = findViewById(R.id.currencyListView);
 
-        final ListView currencyListView = findViewById(R.id.currencyListView);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, currencyOptions);
         currencyListView.setAdapter(adapter);
 
@@ -31,45 +33,73 @@ public class MainActivity extends Activity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 exchangeRate = getExchangeRate(currencyOptions[position]);
+                convertAmount();
             }
         });
 
-        convertButton.setOnClickListener(new View.OnClickListener() {
+        liraAmountEditText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                String liraAmountStr = liraAmountEditText.getText().toString();
-                if (!liraAmountStr.isEmpty()) {
-                    double liraAmount = Double.parseDouble(liraAmountStr);
-                    double convertedAmount = liraAmount * exchangeRate;
-                    convertedAmountEditText.setText(String.format("%.2f", convertedAmount));
-                }
+            public void beforeTextChanged(CharSequence charSequence, int start, int before, int count) {
+                // Leave it empty
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                // Leave it empty
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                convertAmount();
             }
         });
 
-        resetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                liraAmountEditText.setText("");
-                convertedAmountEditText.setText("");
-            }
-        });
+        final TextView titleText = findViewById(R.id.titleText);
+        animateTitleText(titleText);
     }
 
     private double getExchangeRate(String selectedCurrency) {
-        // Bu kısmı doldurmanız gerekiyor.
-        // Seçilen para birimine göre gerçek döviz kurlarını almanız gerekiyor.
-        // Örnek olarak, sabit döviz kurları kullanabilirsiniz (örneğin, USD için 13.0).
         if (selectedCurrency.equals("USD")) {
-            return 13.0;
+            return 18.5;
         } else if (selectedCurrency.equals("EUR")) {
-            return 15.0;
+            return 18.3;
         } else if (selectedCurrency.equals("GBP")) {
-            return 18.0;
+            return 21.1;
         } else if (selectedCurrency.equals("YEN")) {
-            return 4.0;
+            return 7.84;
         } else {
-            return 0.0; // Eğer geçerli bir para birimi yoksa, sıfırı döndürün.
+            return 0.0;
         }
     }
-}
 
+    private void convertAmount() {
+        EditText liraAmountEditText = findViewById(R.id.liraAmount);
+        EditText convertedAmountEditText = findViewById(R.id.convertedAmount);
+
+        String liraAmountStr = liraAmountEditText.getText().toString();
+        if (!liraAmountStr.isEmpty()) {
+            double liraAmount = Double.parseDouble(liraAmountStr);
+            double convertedAmount = liraAmount * exchangeRate;
+            convertedAmountEditText.setText(String.format("%.2f", convertedAmount));
+        }
+    }
+
+    private void animateTitleText(final TextView titleText) {
+        int yellowColor = 0xFFFF00FF; // Yellow color in ARGB format
+        int pinkColor = 0xFFFA58F4; // Pink color in ARGB format
+
+        ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), pinkColor, yellowColor);
+        colorAnimation.setDuration(2000); // 2000 milliseconds or 2 seconds
+
+        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                titleText.setTextColor((int) animator.getAnimatedValue());
+            }
+        });
+
+        colorAnimation.setRepeatMode(ValueAnimator.REVERSE);
+        colorAnimation.setRepeatCount(ValueAnimator.INFINITE);
+        colorAnimation.start();
+    }
+}
